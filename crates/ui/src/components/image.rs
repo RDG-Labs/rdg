b35de3c448baf2db542a4a224e33dfcc7950ea15
corneliustantius.ct@gmail.com <corneliustantius.ct@gmail.com>
2026-09-01 00:51:20 +0700

    Replace Rdg application logos

--- crates/ui/src/components/image.rs
+++ crates/ui/src/components/image.rs
@@ -19,7 +19,6 @@ pub enum VectorName {
     ProTrialStamp,
     ProUserStamp,
     StudentStamp,
-    ZedLogo,
     ZedXCopilot,
 }
 
@@ -122,7 +121,7 @@ impl Component for Vector {
                     vec![
                         single_example(
                             "Default",
-                            Vector::square(VectorName::ZedLogo, size).into_any_element(),
+                            Vector::square(VectorName::BusinessStamp, size).into_any_element(),
                         ),
                         single_example(
                             "Custom Size",
@@ -130,7 +129,7 @@ impl Component for Vector {
                                 .h(rems_from_px(120_f32))
                                 .justify_center()
                                 .child(Vector::new(
-                                    VectorName::ZedLogo,
+                                    VectorName::BusinessStamp,
                                     rems_from_px(120_f32),
                                     rems_from_px(200_f32),
                                 ))
@@ -143,13 +142,13 @@ impl Component for Vector {
                     vec![
                         single_example(
                             "Accent Color",
-                            Vector::square(VectorName::ZedLogo, size)
+                            Vector::square(VectorName::BusinessStamp, size)
                                 .color(Color::Accent)
                                 .into_any_element(),
                         ),
                         single_example(
                             "Error Color",
-                            Vector::square(VectorName::ZedLogo, size)
+                            Vector::square(VectorName::BusinessStamp, size)
                                 .color(Color::Error)
                                 .into_any_element(),
                         ),
@@ -174,6 +173,9 @@ mod tests {
 
     #[test]
     fn vector_path() {
-        assert_eq!(VectorName::ZedLogo.path().as_ref(), "images/zed_logo.svg");
+        assert_eq!(
+            VectorName::BusinessStamp.path().as_ref(),
+            "images/business_stamp.svg"
+        );
     }
 }
